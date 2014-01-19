44f9a98f638aa0cb7514057a4a8c28867bd641d7
Andrea Herbas <andyhq@gmail.com>
2014-01-19 11:41:18 -0400

    Fix issue in core data related to a bad type for id

--- myTracker/Model/_RemoteObject.m
+++ myTracker/Model/_RemoteObject.m
@@ -41,11 +41,6 @@ const struct RemoteObjectFetchedProperties RemoteObjectFetchedProperties = {
 + (NSSet*)keyPathsForValuesAffectingValueForKey:(NSString*)key {
 	NSSet *keyPaths = [super keyPathsForValuesAffectingValueForKey:key];
 	
-	if ([key isEqualToString:@"idValue"]) {
-		NSSet *affectingKey = [NSSet setWithObject:@"id"];
-		keyPaths = [keyPaths setByAddingObjectsFromSet:affectingKey];
-		return keyPaths;
-	}
 
 	return keyPaths;
 }
@@ -62,25 +57,6 @@ const struct RemoteObjectFetchedProperties RemoteObjectFetchedProperties = {
 
 @dynamic id;
 
-
-
-- (int16_t)idValue {
-	NSNumber *result = [self id];
-	return [result shortValue];
-}
-
-- (void)setIdValue:(int16_t)value_ {
-	[self setId:[NSNumber numberWithShort:value_]];
-}
-
-- (int16_t)primitiveIdValue {
-	NSNumber *result = [self primitiveId];
-	return [result shortValue];
-}
-
-- (void)setPrimitiveIdValue:(int16_t)value_ {
-	[self setPrimitiveId:[NSNumber numberWithShort:value_]];
-}
 
 
 
